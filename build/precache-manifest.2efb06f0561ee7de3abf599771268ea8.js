037ee6e6567c1f3f6e6107de22fcecf707ca9813
Sai Vaikant Reddy <reddybgsaivaikant@gmail.com>
2023-10-19 01:20:44 +0530

    First changes
    My first changes

---
+++ build/precache-manifest.2efb06f0561ee7de3abf599771268ea8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4332db2d25bc176eeb40",
+    "url": "/static/js/main.86cc66b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85c1e10d4dcc6f845e70",
+    "url": "/static/js/2.6b0d7161.chunk.js"
+  },
+  {
+    "revision": "ff03d895c4a36eef73c38d77c57b4c5c",
+    "url": "/index.html"
+  }
+];
